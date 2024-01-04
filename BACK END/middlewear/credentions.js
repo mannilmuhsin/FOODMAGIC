5c907b04c5072b3c918d6ea67d7e67494ce023ca
mannilmuhsin <mannilmuhsin3@gmail.com>
2024-01-04 12:39:23 +0530

    some changes

--- BACK END/middlewear/credentions.js
+++ BACK END/middlewear/credentions.js
@@ -2,6 +2,7 @@ const allowedOrigins = require('../config/allowdOrgins');
 
 const credentials = (req, res, next) => {
     const origin = req.headers.origin;
+    console.log('credntion',origin);
     if (allowedOrigins.includes(origin)) {
         res.header('Access-Control-Allow-Credentials', true);
     }
