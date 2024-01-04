f3948e35ca0e4c92c993bbd0e6666ce9083cc66b
mannilmuhsin <mannilmuhsin3@gmail.com>
2024-01-04 13:01:49 +0530

    some chagnes

--- BACK END/middlewear/credentions.js
+++ BACK END/middlewear/credentions.js
@@ -4,6 +4,7 @@ const credentials = (req, res, next) => {
     const origin = req.headers.origin;
     console.log('credntion',origin);
     if (allowedOrigins.includes(origin)) {
+        console.log('on credention includes importsnt')
         res.header('Access-Control-Allow-Credentials', true);
     }
     console.log(req.headers);
