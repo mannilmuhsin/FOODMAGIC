98c9bb9db4bdc80bae401ab211510543fe155677
mannilmuhsin <mannilmuhsin3@gmail.com>
2024-01-04 14:38:54 +0530

    some changes

--- BACK END/config/corsOptions.js
+++ BACK END/config/corsOptions.js
@@ -1,18 +1,19 @@
-
-const allowedOrigins = require('./allowdOrgins');
+const allowedOrigins = require("./allowdOrgins");
 
 const corsOptions = {
-    origin: (origin, callback) => {
-        console.log(origin)
-        if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
-            console.log('allowed ');
-            callback(null, true)
-        } else {
-            console.log('not allowed ');
-            callback(new Error('Not allowed by CORS'));
-        }
-    },
-    optionsSuccessStatus: 200
-}
+  origin: (origin, callback) => {
+    console.log(origin);
+    if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
+      console.log("allowed ");
+      callback(null, true);
+    } else {
+      console.log("not allowed ");
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+  optionsSuccessStatus: 200,
+  credentials: true,
+  origin: true,
+};
 
 module.exports = corsOptions;
