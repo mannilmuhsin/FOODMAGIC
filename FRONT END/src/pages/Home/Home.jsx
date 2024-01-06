08ded3e6321e799e810abb66d4a0744374ab3cda
mannilmuhsin <mannilmuhsin3@gmail.com>
2024-01-06 15:06:05 +0530

    some ui updation

--- FRONT END/src/pages/Home/Home.jsx
+++ FRONT END/src/pages/Home/Home.jsx
@@ -144,7 +144,8 @@ function Home() {
           >
             <img
               src={chef?.pic?.url}
-              className="w-full h-60  object-cover mb-4"
+              className="w-full h-80  object-cover mb-4"
+              style={{ objectPosition: 'top' }} 
             />
             <h3 className="text-xl font-semibold mb-2">{chef.username}</h3>
           </motion.div>
