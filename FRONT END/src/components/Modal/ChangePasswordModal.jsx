e574bba6642377efcd29859c089b6bf106bf6e39
mannilmuhsin <mannilmuhsin3@gmail.com>
2024-01-09 19:42:17 +0530

    some ui updation

--- FRONT END/src/components/Modal/ChangePasswordModal.jsx
+++ FRONT END/src/components/Modal/ChangePasswordModal.jsx
@@ -104,7 +104,7 @@ function ChangePasswordModal({ setOpenModal }) {
     <>
       <Toaster />
       <div className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none">
-        <div className="relative w-auto my-6 mx-auto max-w-3xl">
+        <div className="relative w-auto bg-red-500 my-6 mx-auto max-w-3xl">
           {/*content*/}
           <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
             {/*header*/}
--- FRONT END/src/pages/User/Profile/Profile.jsx
+++ FRONT END/src/pages/User/Profile/Profile.jsx
@@ -116,7 +116,7 @@ function Profile() {
         <div className="max-w-4xl flex items-center h-auto lg:h-screen flex-wrap mx-auto my-32 lg:my-0">
           <div
             id="profile"
-            className="w-full lg:w-3/5 rounded-lg lg:rounded-l-lg lg:rounded-r-none shadow-2xl bg-blue-200 opacity-75 mx-6 lg:mx-0"
+            className="w-full lg:w-3/5 rounded-lg lg:rounded-l-lg lg:rounded-r-none shadow-2xl bg-blue-200  mx-6 lg:mx-0"
           >
             <div className="p-4 md:p-12 text-center lg:text-left">
               <div
